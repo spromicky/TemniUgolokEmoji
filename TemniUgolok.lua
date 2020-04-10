f56d352a41fd81457bf8d01b46d94ea7fbde1afa
spromicky <spromicky@gmail.com>
2020-04-10 18:57:06 +0300

    lgbt added

--- TemniUgolok.lua
+++ TemniUgolok.lua
@@ -117,6 +117,7 @@ Emoticons_Settings={
     ["taktikaGalaktika"]="Interface\\AddOns\\TemniUgolok\\resources\\taktikaGalaktika.tga",
     ["opyatLokalka"]="Interface\\AddOns\\TemniUgolok\\resources\\opyatLokalka.tga",
     ["plusAhatini"]="Interface\\AddOns\\TemniUgolok\\resources\\plusAhatini.tga",
+    ["lgbt"]="Interface\\AddOns\\TemniUgolok\\resources\\lgbt.tga",
   };
   local emoticons={
     --Filename
@@ -299,11 +300,21 @@ Emoticons_Settings={
     [":pinus:"]="sheal",
     [":pinos:"]="sheal",
     [":penis:"]="sheal",
+    --lgbt
+    ["lgbt"]="lgbt",
+    ["pidor"]="lgbt",
+    ["pidoras"]="lgbt",
+    ["pedik"]="lgbt",
+    ["pidr"]="lgbt",
+    ["пидор"]="lgbt",
+    ["пидорас"]="lgbt",
+    ["педик"]="lgbt",
+    ["пидр"]="lgbt",
   };
   local dropdown_options={
   
      [01]=  {"Discord-style",":bdum_tss:",":bdumtss:",":BURN:",":damag_nahui_ne_nuzhen:",":dc_priest:",":pomoika:",":derzhi_v_kurse:",":double_tilt:",":duble_tilt:",":FeelsBadMan:",":fufaika_stasa:",":gloom:",":gloom_chmok:",":GloomChmok:",":hmm:",":jirok_uprls:",":Kappapride:",":kovanich:",":mirkusha:",":molodchaga:",":monkas:",":ne_shuti_tak:",":ohueli_sovsem:",":olegalul:",":ooooo:",":pepeclown:",":opa:",":raintru_nekaef:",":shto:",":PEPEGA:",":SADBOY:",":slowpoke:",":sps_blizzard:",":zachem_obidel:",":raintru_kaef:",":zemlya_puhom:",":wut:",":PogChamp:",":raintru_ebalo:",":raintru:",":Jason:",":lovushka:",":lovushkaJerokera:",":neponimayu:",":ponimayu:",":sho:",":oshotik_zaebal:",":rfiend:",":fiend:",":known:",":taktika_galaktika:",":opyat_lokalka:",":plus_ahatini:"},
-	 [02]=  {"Названия файла","bdumtss","burn","damagnahuinenuzhen","dcpriest","derzhivkurse","doubletilt","feelsbadman","fufaikastasa","gloom","gloomchmok","hmm","jirokuprls","kappapride","kovanich","mirkusha","molodchaga","monkas","neshutitak","ohuelisovsem","olegalul","ooooooo","opa","pepeclown","pepega","pogchamp","raintru","raintruebalo","raintrukaef","raintrunekaef","sadboy","shto","slowpoke","spsblizzard","wut","zachemobidel","zemlyapuhom","jason","lovushka","lovushkajerokera","neponimayu","ponimayu","sho","oshotikzaebal","rfiend","fiend","know","taktikagalaktika","opyatlokalka","plusahatini"},
+	 [02]=  {"Названия файла","bdumtss","burn","damagnahuinenuzhen","dcpriest","derzhivkurse","doubletilt","feelsbadman","fufaikastasa","gloom","gloomchmok","hmm","jirokuprls","kappapride","kovanich","mirkusha","molodchaga","monkas","neshutitak","ohuelisovsem","olegalul","ooooooo","opa","pepeclown","pepega","pogchamp","raintru","raintruebalo","raintrukaef","raintrunekaef","sadboy","shto","slowpoke","spsblizzard","wut","zachemobidel","zemlyapuhom","jason","lovushka","lovushkajerokera","neponimayu","ponimayu","sho","oshotikzaebal","rfiend","fiend","know","taktikagalaktika","opyatlokalka","plusahatini","lgbt"},
      [03]=  {"Сокращения",":bdum:",":dnnn:",":dc:",":dvk:",":dt:",":fbm:",":fs:",":gc:",":ju:",":kp:",":mldg:",":nst:",":os:",":olul:",":pc:",":rnk:",":sps:",":zo:",":rk:",":zp:",":pch:",":re:",":oz:",":jsn:",":lsh:",":lshj:",":npnm:",":pnm:",":tg:",":op:",":pa:"},
 };
   
